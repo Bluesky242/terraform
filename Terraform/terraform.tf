8852df6686c3fccc329b665ed12bdc758b4760c7
Bluesky242 <dondomsky@gmail.com>
2023-11-16 21:06:49 +0100

    Ajouter le dossier Terraform et le fichier .gitignore

---
+++ Terraform/terraform.tf
@@ -0,0 +1,40 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "arg"
+  location = "West Europe"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "as" {
+  name                 = "as"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "aks"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "exampleaks1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "local_file" "kube_config" {
+  content  = azurerm_kubernetes_cluster.aks.kube_config_raw
+  filename = ".kube/config"
+}
